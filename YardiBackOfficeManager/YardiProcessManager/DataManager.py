9f70fac783d5d892ee3dcab1755d89c8c95ad72a
brandonmichaelhunter <brandonmichaelhunter@live.com>
2017-08-24 22:09:19 -0400

    Connecting to firebase
    Add code to connect to firebase

--- YardiBackOfficeManager/YardiProcessManager/DataManager.py
+++ YardiBackOfficeManager/YardiProcessManager/DataManager.py
@@ -3,19 +3,38 @@
 #
 import urllib3
 import socket
+import pyrebase
 class DataManager(object):
 
+    config = ""
+    firebase = ""
     def __init__(self):
+        self.config = {
+          "apiKey": "AIzaSyDdd2opaT2VZUkSuhwWuM0Kcz6gRJUR_e0",
+          "authDomain": "yardiai-4ca2b.firebaseapp.com",
+          "databaseURL": "https://yardiai-4ca2b.firebaseio.com",
+          "storageBucket": "yardiai-4ca2b.appspot.com",
+          "serviceAccount": "C:\\Users\Brandon\Documents\GitHub\YardiAI\YardiAI\YardiBackOfficeManager\YardiProcessManager\Resources\YardiAI-303194ef51f7.json"
+        }
+        self.firebase = pyrebase.initialize_app(self.config)
         pass
 
+
     # Determines if we're connected to firebase.
-    def _IsConnected(self):
+    def IsConnected(self):
         # This method will see if we're connected online.
        try:
-        # connect to the host -- tells us if the host is actually
-        # reachable
+        # connect to the host -- tells us if the host is actually reachable
         socket.create_connection(("firebase.google.com", 80))
         return True
        except OSError:
         return False
+
+    def GetRecentPosts(self):
+        
+        db = self.firebase.database()
+        recs = db.child("Commands").get()
+        return recs
+        
+
 
--- YardiBackOfficeManager/YardiProcessManager/YardiProcessManager.py
+++ YardiBackOfficeManager/YardiProcessManager/YardiProcessManager.py
@@ -2,5 +2,8 @@ import DataManager
 
 dm = DataManager.DataManager()
 IsConnected = dm.IsConnected()
-
+dbRecs = dm.GetRecentPosts()
 print("Is connected {}.".format(IsConnected))
+for item in dbRecs.each():
+    print(item.key()) # Morty
+    print(item.val()) # {name": "Mortimer 'Morty' Smith"}
