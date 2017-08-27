b8fba19d9babf2079bc720c028afb2a5a3511693
brandonmichaelhunter <brandonmichaelhunter@live.com>
2017-08-26 20:56:03 -0400

    Added Bootstrap
    Added bootstrap as my web template

---
+++ YardiBackOfficeManager/YardiAIWeb/js/FirebaseInit.js
@@ -0,0 +1,10 @@
+﻿// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDdd2opaT2VZUkSuhwWuM0Kcz6gRJUR_e0",
+    authDomain: "yardiai-4ca2b.firebaseapp.com",
+    databaseURL: "https://yardiai-4ca2b.firebaseio.com",
+    projectId: "yardiai-4ca2b",
+    storageBucket: "yardiai-4ca2b.appspot.com",
+    messagingSenderId: "295023850130"
+};
+firebase.initializeApp(config);
