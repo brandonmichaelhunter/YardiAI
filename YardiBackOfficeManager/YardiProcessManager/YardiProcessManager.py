e9ba93dc296ccc7d16abb39275545b7a10d14db1
brandonmichaelhunter <brandonmichaelhunter@live.com>
2017-09-04 17:16:39 -0400

    Process recs from Firebase
    #7 Updated code base to process new records added to the YardTasks table

--- YardiBackOfficeManager/YardiProcessManager/YardiProcessManager.py
+++ YardiBackOfficeManager/YardiProcessManager/YardiProcessManager.py
@@ -1,9 +1,11 @@
+#TODO:
+# Add unit test -check out these articles - http://www.blog.pythonlibrary.org/2011/03/09/python-102-an-intro-to-tdd-and-unittest/ and http://www.blog.pythonlibrary.org/2016/07/07/python-3-testing-an-intro-to-unittest/
+# 
 import DataManager
 
 dm = DataManager.DataManager()
 IsConnected = dm.IsConnected()
-dbRecs = dm.GetRecentPosts()
-print("Is connected {}.".format(IsConnected))
-for item in dbRecs.each():
-    print(item.key()) # Morty
-    print(item.val()) # {name": "Mortimer 'Morty' Smith"}
+dm.CallerGetRecentUnprocessTasks()
+#UnprocessedYardTasks = dm.GetRecentPosts()
+#print("Is connected {}.".format(IsConnected))
+#print(UnprocessedYardTasks)
