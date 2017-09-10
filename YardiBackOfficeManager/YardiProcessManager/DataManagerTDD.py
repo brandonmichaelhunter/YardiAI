9de5159c9c59587e03e3742f179970a3109fb80d
brandonmichaelhunter <brandonmichaelhunter@live.com>
2017-09-10 19:50:34 -0400

    updated gitinore file

--- YardiBackOfficeManager/YardiProcessManager/DataManagerTDD.py
+++ YardiBackOfficeManager/YardiProcessManager/DataManagerTDD.py
@@ -5,24 +5,24 @@ class Test_DataManagerTDD(unittest.TestCase):
     
     def test_LoadTestData(self):
         # Delete any existing files in the test images folder
-        #TestImageLocation = "C:\\FirebaseTestImages\\Images"
-        #OldImagesList = os.listdir(TestImageLocation)
-        #for ImageItem in OldImagesList:
-        #    os.unlink("{}\\{}".format(TestImageLocation,ImageItem))
+        TestImageLocation = "C:\\FirebaseTestImages\\Images"
+        OldImagesList = os.listdir(TestImageLocation)
+        for ImageItem in OldImagesList:
+            os.unlink("{}\\{}".format(TestImageLocation,ImageItem))
          
-        #fbTestLoader = FirebaseTestDataLoader.FirebaseTestDataLoader();
+        fbTestLoader = FirebaseTestDataLoader.FirebaseTestDataLoader();
         # Test the LoadTestData method and have it create 10 images.
-        #ExpectedCreatedImages = 10
-        #fbTestLoader.LoadTestData(ExpectedCreatedImages,False)
+        ExpectedCreatedImages = 10
+        fbTestLoader.LoadTestData(ExpectedCreatedImages,False)
 
         #Setup test configuration for testing against the LoadTestData method.
         
-        #ActualImagesCreated = 0
-        #ImagesList = os.listdir(TestImageLocation)
-        #for ImageItem in ImagesList:
-        #    ActualImagesCreated += 1
+        ActualImagesCreated = 0
+        ImagesList = os.listdir(TestImageLocation)
+        for ImageItem in ImagesList:
+            ActualImagesCreated += 1
 
-        self.assertEqual(1,1)
+        self.assertEqual(ExpectedCreatedImages,ActualImagesCreated)
 
 if __name__ == '__main__':
     unittest.main()
